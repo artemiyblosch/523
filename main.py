758d29200eb2af35e777a2613b3873d26f77db43
arti <arti@privategatevpn.com>
2024-12-10 23:23:54 +0300

    +Меню

--- main.py
+++ main.py
@@ -23,16 +23,25 @@ class Block:
         pygame.draw.rect(screen,(0,0,0),\
             pygame.Rect(self.x_b,self.y_b,self.x_e - self.x_b,self.y_e - self.y_b))
 
+pygame.font.init()
 pygame.init()
 
 screen = pygame.display.set_mode((400, 400))
 clock = pygame.time.Clock()
+font1 = pygame.font.Font('freesansbold.ttf', 32)
+font2 = pygame.font.Font('freesansbold.ttf', 12)
 
-hardmode = True
+hardmode = False
+slope = -1
 fps = 360
 player_y = 200
-velocity = -100/fps
+velocity = -100/fps*slope
 blocks = [generate_at(400+200*i) for i in range(20 if hardmode else 10)]
+menu_mode = True
+
+title = font1.render("523.py", False, (255,255,255))
+titleRect = title.get_rect()
+titleRect.center = (200,50)
 
 on_ground = 0
 while True:
@@ -40,15 +49,35 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-        if event.type == pygame.KEYDOWN:
-            velocity = 100/fps
-        if event.type == pygame.KEYUP:
-            velocity = -100/fps
+
+    k = pygame.key.get_pressed()
+    if k[pygame.K_SPACE] or k[pygame.K_UP]:
+        velocity = 100/fps*slope
+    else:
+        velocity = -100/fps*slope
+    if menu_mode and k[pygame.K_h]:
+        hardmode = not hardmode
+    elif menu_mode and k[pygame.K_RETURN]: menu_mode = False
     
+    if menu_mode:
+        pygame.draw.rect(screen,(0,0,0),pygame.Rect(0,0,400,400),0)
+        screen.blit(title,titleRect)
+        hm = font2.render(f"Hardmode(H to toggle): {hardmode}", False, (255,255,255))
+        hmRect = title.get_rect()
+        hmRect.center = (200,150)
+        screen.blit(hm,hmRect)
+        pygame.display.flip()
+        continue
+
     player_y += velocity
     pygame.draw.rect(screen,(255,255,255),pygame.Rect(0,0,400,400),0)
-    pygame.draw.polygon(screen,(0,0,0),[(95,player_y-5),(105,player_y),(95,player_y+5)])
-
+    if velocity == 0:
+        pygame.draw.polygon(screen,(0,0,0),[(95,player_y-5),(105,player_y),(95,player_y+5)])
+    elif velocity > 0:
+        pygame.draw.polygon(screen,(0,0,0),[(92,player_y+3),(98,player_y - 3),(95+7,player_y+7)])
+    elif velocity < 0:
+        pygame.draw.polygon(screen,(0,0,0),[(92,player_y-3),(95+7,player_y - 7),(98,player_y+3)])
+    
     for index,i in enumerate(blocks):
         i.x_b -= 100/fps
         i.x_e -= 100/fps
@@ -63,12 +92,14 @@ while True:
             pygame.quit()
             sys.exit()
 
-    if player_y < 5:
+    if player_y <= 5:
         player_y = 5
         on_ground += 1/fps
-    elif player_y > 395: 
+        velocity = 0
+    elif player_y >= 395: 
         player_y = 395
         on_ground += 1/fps
+        velocity = 0
     
     if on_ground >= (0.75 if hardmode else 1.5):
         blocks.append(Block(450,0,1450,25))
